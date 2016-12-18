06427ffc3cf4ac4eda071886b13133d06703195f
samuel2017 <samuel2017@xxx.xxx>
2016-12-18 14:13:00 +0800

    add validation to title apply to edit/update 2st time

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,20 +15,25 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
    end
 
-  def create
+   def create
    @group = Group.new(group_params)
-   @group.save
 
+   if @group.save
      redirect_to groups_path
+   else
+     render :new
+   end
  end
 
-   def update
-     @group = Group.find(params[:id])
-
-     @group.update(group_params)
-
-     redirect_to groups_path, notice: "Update Success"
-   end
+ def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+  
 
      def destroy
        @group = Group.find(params[:id])
